9e0a7493e66b750be49f445fabd85c0777a5cbb2
Egorov-ops <evladislav489@gmail.com>
2023-06-06 23:39:52 +0500

    Creating register and sign in fragments

--- app/src/main/java/com/example/rentcars/data/Functions.kt
+++ app/src/main/java/com/example/rentcars/data/Functions.kt
@@ -7,46 +7,70 @@ import com.example.rentcars.data.entity.ProfileEntity
 import com.example.rentcars.data.entity.StateOfCar
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
 
-fun getCars(): List<CarEntity> {
+suspend fun getCars(profileEntity: ProfileEntity, carEntity: CarEntity): List<CarEntity> {
     val db = Firebase.firestore
+    val userId = profileEntity.id.toString()
 
-    return emptyList()
+    val carList = mutableListOf<CarEntity>()
+
+    val querySnapshot = db.collection("Cars")
+        .whereEqualTo("userId", userId)
+        .get()
+        .await() // Добавляем await(), чтобы дождаться завершения операции получения данных
+
+    for (documentSnapshot in querySnapshot.documents) {
+        // Получите данные машины из документа
+        val carData = documentSnapshot.data
+
+        // Создайте объект Car и добавьте его в список
+        val car = carEntity
+
+        // Добавьте другие поля машины по необходимости
+
+        carList.add(carEntity)
+    }
+
+    return carList
 }
 
-fun addCars(carEntity: CarEntity) {
-    val db = Firebase.firestore
-    db.collection("Cars").document(carEntity.markAndModel).set(carEntity)
-        .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-        .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+    fun addCars(carEntity: CarEntity) {
+        val db = Firebase.firestore
+        db.collection("Cars").document(carEntity.markAndModel).set(carEntity)
+            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
 
-}
+    }
 
-fun deleteCar(carEntity: CarEntity) {
-    val db = Firebase.firestore
-    db.collection("Cars").document(carEntity.markAndModel).delete()
-        .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully deleted!") }
-        .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
-}
+    fun deleteCar(carEntity: CarEntity) {
+        val db = Firebase.firestore
+        db.collection("Cars").document(carEntity.markAndModel).delete()
+            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully deleted!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
+    }
 
-fun editStateCar(carEntity: CarEntity,newState :CarEntity) {
-    val db = Firebase.firestore
-    db.collection("Cars").document(carEntity.state.toString()).update(carEntity.state.toString(),newState)
-}
+    fun editStateCar(carEntity: CarEntity, newState: CarEntity) {
+        val db = Firebase.firestore
+        db.collection("Cars").document(carEntity.state.toString())
+            .update(carEntity.state.toString(), newState)
+    }
 
-fun editProfile(profileEntity: ProfileEntity,newName:String) {
-    /* Видимо с binding тягать данные будешь я хз как параметры их передавать , в комментах чтобы ошибок не было,
-     там допиши параметры передаваемые
-    потому что я хз что туда передать , написал как пример
-    */
-    val db = Firebase.firestore
-    db.collection("Users").document(profileEntity.name).update(profileEntity.name,newName)
-    //db.collection("Users").document(profileEntity.phone).update(profileEntity.phone)
-    //db.collection("Users").document(profileEntity.region).update(profileEntity.region)
+    fun editProfile(profileEntity: ProfileEntity, newName: String) {
+        /* Видимо с binding тягать данные будешь я хз как параметры их передавать , в комментах чтобы ошибок не было,
+         там допиши параметры передаваемые
+        потому что я хз что туда передать , написал как пример
+        */
+        val db = Firebase.firestore
+        db.collection("Users").document(profileEntity.name).update(profileEntity.name, newName)
+        //db.collection("Users").document(profileEntity.phone).update(profileEntity.phone)
+        //db.collection("Users").document(profileEntity.region).update(profileEntity.region)
 
 
-}
+    }
+/*
+
 
 
 /*
